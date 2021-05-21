cc0c3e3f800a858fa03a6deacc659964844bea89
lorneez <lornz17@gmail.com>
2021-05-20 23:14:07 -0700

    refactored getEntryData to use async await

--- lambda/getEntryData/index.js
+++ lambda/getEntryData/index.js
@@ -6,19 +6,21 @@ AWS.config.update({ region: "us-west-1" });
 // event contains information about request (event that triggered lambda)
 // context contains information about invocation, function, execution environment
 // callback is method that returns to calling function that invokes lambda
-exports.handler = function (event, context, callback) {
+exports.handler = async (event, context) => {
     const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" }); // instance of DynamoDB
-    const documentClient = new AWS.DynamoDB.DocumentClient({ region: "us-west-1" }); // Document client
+    const documentClient = new AWS.DynamoDB.DocumentClient({ region: "us-west-1" }); // Document client to simulate more traditional JSON syntax
+
     const params = {
         TableName: "Mini-Blog",
         Key: {
             id: "12345"
         }
     };
-    documentClient.get(params, (err, data) => {
-        if(err) {
-            console.log(err);
-        }
+
+    try {
+        const data = await documentClient.get(params).promise();
         console.log(data);
-    });
+    } catch (err) {
+        console.log(err);
+    }
 }
