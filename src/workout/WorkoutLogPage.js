ef3664b5c7b0eee33561ad5108dddead74313de8
lorneez <lornz17@gmail.com>
2021-05-22 02:46:03 -0700

    added graph and completed frontend

--- src/workout/WorkoutLogPage.js
+++ src/workout/WorkoutLogPage.js
@@ -3,11 +3,13 @@ import "../App.css";
 import ProfileComponent from "../home/ProfileComponent";
 import axios from 'axios';
 import FitnessEntryComponent from "./FitnessEntryComponent";
-
+import LineChart from 'react-linechart';
+import '../../node_modules/react-linechart/dist/styles.css';
 
 function WorkoutLogPage() {
 
     const [workouts, setWorkouts] = useState([]);
+    const [buckets, setBuckets] = useState([]);
 
     const [Database, setDatabase] = useState("");
     const [LogType, setLogType] = useState("");
@@ -15,33 +17,68 @@ function WorkoutLogPage() {
     const [GymType, setGymType] = useState("");
     const [MealType, setMealType] = useState("");
 
+    const [RangeDuration, setRangeDuration] = useState("");
+    const [RangeValue, setRangeValue] = useState("");
+    const [BucketDuration, setBucketDuration] = useState("");
+
     function generateFitnessAPICall() {
-        if(LogType === "") {
-            return `https://j0zcpa8hsk.execute-api.us-west-1.amazonaws.com/prod/workout?LogType=`;
+        // must require LogType, Type, Location, RangeDuration, RangeValue, and BucketDuration
+        let URL;
+        if(LogType === "Gym") {
+            URL = `https://j0zcpa8hsk.execute-api.us-west-1.amazonaws.com/prod/workout?LogType=Gym&Type=`
+                + GymType
+                + "&Location="
+                + GymLocation
+                + "&RangeDuration="
+                + RangeDuration
+                + "&RangeValue="
+                + RangeValue
+                + "&BucketDuration="
+                + BucketDuration
+            ;
         }
-        else {
-            if(LogType === "Gym") {
-                return `https://j0zcpa8hsk.execute-api.us-west-1.amazonaws.com/prod/workout?LogType=Gym&Type=` + GymType + "&Location=" + GymLocation;
-            }
-            else if(LogType === "Meal") {
-                return `https://j0zcpa8hsk.execute-api.us-west-1.amazonaws.com/prod/workout?LogType=Meal&Type=` + MealType;
-            }
+        else if(LogType === "Meal") {
+            URL = `https://j0zcpa8hsk.execute-api.us-west-1.amazonaws.com/prod/workout?LogType=Meal&Type=`
+                + MealType
+                + "&RangeDuration="
+                + RangeDuration
+                + "&RangeValue="
+                + RangeValue
+                + "&BucketDuration="
+                + BucketDuration
+            ;
         }
+        return URL;
     }
 
     useEffect(() => {
-        if(Database === "Fitness") {
+        if(Database === "Fitness" && LogType !== "") {
+            console.log(generateFitnessAPICall())
             axios.get(generateFitnessAPICall())
                 .then(res => {
-                    console.log(res.data)
-
-                    // TODO: Set up sorting
-                    const sortedWorkouts = res.data.sort((a, b) => Date.parse(b.Date) - Date.parse(a.Date))
-                    setWorkouts(res.data)
-
+                    console.log(res.data);
+                    setWorkouts(res.data.results);
+                    let tempBuckets = [];
+                    tempBuckets.push({
+                        x: res.data.buckets.length,
+                        y: 0
+                    })
+                    for(let i=0; i<res.data.buckets.length; i++) {
+                        tempBuckets.push(
+                            {
+                                x: res.data.buckets.length - i - 1,
+                                y: res.data.buckets[i]
+                            }
+                        )
+                    }
+                    setBuckets(tempBuckets);
                 })
         }
-    }, [Database, LogType, GymLocation, GymType, MealType]);
+        else {
+            setWorkouts([]);
+            setBuckets([]);
+        }
+    }, [Database, LogType, GymLocation, GymType, MealType, RangeDuration, RangeValue, BucketDuration]);
 
     function renderTypeSelection() {
         if(Database === "") {
@@ -56,6 +93,9 @@ function WorkoutLogPage() {
                             setGymType("");
                             setGymLocation("");
                             setMealType("");
+                            setBucketDuration("");
+                            setRangeValue("");
+                            setRangeDuration("");
                         }}>
                             <option value="">Log Type</option>
                             <option value="Gym">Gym</option>
@@ -114,6 +154,119 @@ function WorkoutLogPage() {
         }
     }
 
+    function renderBucketSelection() {
+        if(Database === "" || LogType === "") {
+            return <div></div>
+        }
+        else {
+            return (
+                <div style={{display: "flex", flexDirection: "column"}}>
+                    <div style={{display: "flex", flexDirection: "row"}}>
+                        <div style={{padding: "10px"}}>
+                            <select className={"select"} value={RangeDuration} onChange={(e) => setRangeDuration(e.target.value)}>
+                                <option value="">Range Duration</option>
+                                <option value="Day">Day</option>
+                                <option value="Week">Week</option>
+                                <option value="Month">Month</option>
+                                <option value="Year">Year</option>
+                            </select>
+                        </div>
+                        <div style={{padding: "10px"}}>
+                            <select className={"select"} value={RangeValue} onChange={(e) => setRangeValue(e.target.value)}>
+                                <option value="">Range Value</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div style={{display: "flex", flexDirection: "row"}}>
+                        <div style={{padding: "10px"}}>
+                            <select className={"select"} value={BucketDuration} onChange={(e) => setBucketDuration(e.target.value)}>
+                                <option value="">Bucket Duration</option>
+                                <option value="Day">Day</option>
+                                <option value="Week">Week</option>
+                                <option value="Month">Month</option>
+                                <option value="Year">Year</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    function renderWorkouts() {
+        if(Database === "" || LogType === "") {
+            return <div></div>
+        }
+        else {
+            return (
+                <div>
+                    <div className={"title"}>
+                        List View
+                    </div>
+                    {workouts.map(workout => (
+                        <FitnessEntryComponent LogType={workout.LogType} Gym={workout.Gym} Meal={workout.Meal} Date={workout.Date} Description={workout.Description}/>
+                    ))}
+                </div>
+            )
+        }
+    }
+
+    function renderBucketGraph() {
+        if(buckets.length === 0) {
+            return <div></div>
+        }
+        else {
+
+            const data = [
+                {
+                    id: "hi",
+                    name: "graph",
+                    color: "#61E294",
+                    points: buckets
+                }
+            ];
+            return (
+                <div>
+                    <div className={"title"}>
+                        {renderBucketTitle()}
+                    </div>
+                    <div style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
+                        <LineChart
+                            interpolate={"none"}
+                            xLabel={"DAYS"}
+                            yLabel={LogType === "Gym" ? "MINUTES IN THE GYM" : "GRAMS OF PROTEIN"}
+                            width={400}
+                            height={400}
+                            data={data}
+                        />
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    function renderBucketTitle() {
+        if(RangeValue === "" || RangeDuration === "" || BucketDuration === "") {
+            return (
+                <div>
+                    Timeline: All past {LogType === "Gym" ? "gym durations" : "protein counts"} bucketed into days
+                </div>
+            )
+        }
+        else {
+            return (
+                <div>
+                    Timeline: Past {RangeValue} {RangeDuration} bucketed into {BucketDuration}s
+                </div>
+            )
+        }
+    }
+
     return (
         <div
             className={"home"}
@@ -154,6 +307,9 @@ function WorkoutLogPage() {
                                             setGymType("");
                                             setGymLocation("");
                                             setMealType("");
+                                            setBucketDuration("");
+                                            setRangeValue("");
+                                            setRangeDuration("");
                                         }}>
                                             <option value="">Database</option>
                                             <option value="Fitness">Fitness</option>
@@ -166,11 +322,15 @@ function WorkoutLogPage() {
                                 <div>
                                     {renderString_1Selection()}
                                 </div>
+                                <div>
+                                    {renderBucketSelection()}
+                                </div>
                             </div>
                             <div>
-                                {workouts.map(workout => (
-                                    <FitnessEntryComponent LogType={workout.LogType} Gym={workout.Gym} Meal={workout.Meal} Date={workout.Date} Description={workout.Description}/>
-                                ))}
+                                {renderBucketGraph()}
+                            </div>
+                            <div>
+                                {renderWorkouts()}
                             </div>
                         </div>
                     </div>
