b669cfe1b26cd713a16c61e66eefa8fbbd2baa72
lorneez <lornz17@gmail.com>
2021-05-21 21:58:53 -0700

    finished frontend display of workouts

--- src/workout/WorkoutLogPage.js
+++ src/workout/WorkoutLogPage.js
@@ -7,12 +7,13 @@ import axios from 'axios';
 function WorkoutLogPage() {
 
     const [workouts, setWorkouts] = useState([]);
+    const [Database, setDatabase] = useState("");
     const [LogType, setLogType] = useState("");
     const [String_1, setString_1] = useState("");
     const [Int_1, setInt_1] = useState("");
 
     useEffect(() => {
-        axios.get(`https://j0zcpa8hsk.execute-api.us-west-1.amazonaws.com/prod/workout?LogType=` + "Meal" + `&String_1=` + "" + `&Int_1=` + "")
+        axios.get(`https://j0zcpa8hsk.execute-api.us-west-1.amazonaws.com/prod/workout?LogType=` + LogType + `&String_1=` + String_1 + `&Int_1=` + Int_1)
             .then(res => {
                 console.log(res.data)
 
@@ -21,7 +22,60 @@ function WorkoutLogPage() {
                 setWorkouts(res.data)
 
             })
-    }, []);
+    }, [LogType, String_1]);
+
+    function renderTypeSelection() {
+        if(Database === "") {
+            return <div></div>
+        }
+        else {
+            switch(Database) {
+                case "Fitness":
+                    return (
+                        <select className={"select"} value={LogType} onChange={(e) => {
+                            setLogType(e.target.value);
+                            setString_1("");
+                            setInt_1("");
+                        }}>
+                            <option value="">Log Type</option>
+                            <option value="Gym">Gym</option>
+                            <option value="Meal">Meal</option>
+                        </select>
+                    )
+                default:
+                    return <div></div>
+            }
+        }
+    }
+
+    function renderString_1Selection() {
+        if(Database === "" || LogType === "") {
+            return <div></div>
+        }
+        else {
+            switch(LogType) {
+                case "Gym":
+                    return (
+                        <select className={"select"} value={String_1} onChange={(e) => setString_1(e.target.value)}>
+                            <option value="">Body Part</option>
+                            <option value="Chest">Chest</option>
+                            <option value="Back">Back</option>
+                        </select>
+                    )
+                case "Meal":
+                    return (
+                        <select className={"select"} value={String_1} onChange={(e) => setString_1(e.target.value)}>
+                            <option value="">Meal of the day</option>
+                            <option value="Breakfast">Breakfast</option>
+                            <option value="Lunch">Lunch</option>
+                            <option value="Dinner">Dinner</option>
+                        </select>
+                    )
+                default:
+                    return <div></div>
+            }
+        }
+    }
 
     return (
         <div
@@ -49,6 +103,30 @@ function WorkoutLogPage() {
                         >
                             <div className={"title"}>
                                 Workout Logs
+                            </div>
+                            <div
+                                className={"my-3"}
+                                style={{display: "flex", flexDirection: "column", background: "#61E294", borderRadius: "5px", padding: "5px"}}
+                            >
+                                <div style={{display: "flex", flexDirection: "row"}}>
+                                    <div style={{padding: "10px"}}>
+                                        <select className={"select"} value={Database} onChange={(e) => {
+                                            setDatabase(e.target.value);
+                                            setString_1("");
+                                            setInt_1("");
+                                            setLogType("");
+                                        }}>
+                                            <option value="">Database</option>
+                                            <option value="Fitness">Fitness</option>
+                                        </select>
+                                    </div>
+                                    <div style={{padding: "10px"}}>
+                                        {renderTypeSelection()}
+                                    </div>
+                                </div>
+                                <div style={{padding: "10px"}}>
+                                    {renderString_1Selection()}
+                                </div>
                             </div>
                             <div>
                                 {workouts.map(workout => (
